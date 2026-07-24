6156f48a7b07ebce55bb54b818f1c14fd107825d
Dimal Kavindu J. <80914328+DimalJay@users.noreply.github.com>
2026-07-24 10:01:13 +0530

    update filter popup

--- components/admin/TableCard.tsx
+++ components/admin/TableCard.tsx
@@ -7,7 +7,7 @@ interface TableCardProps {
 export default function TableCard({ children }: TableCardProps) {
   return (
     <div
-      className="bg-white/80 backdrop-blur-md rounded-3xl border border-zinc-200/60 shadow-xs overflow-hidden"
+      className="bg-white/80 backdrop-blur-md rounded-3xl border border-zinc-200/60 shadow-xs"
     >
       {children}
     </div>
