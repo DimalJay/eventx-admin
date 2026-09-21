5372772ce1fb5aeb01d575835a68a930dd49621f
H. S. L. Silva <sandaralasandi03@gmail.com>
2026-09-21 12:06:14 +0530

    feat: add AdminSidebar component with navigation and logout functionality

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -19,8 +19,7 @@ import {
 } from "lucide-react";
 import Logo from "@/components/widgets/Logo";
 import { cn } from "@/lib/utils";
-import { useRouter } from "next/navigation";
-import { logoutRequest } from "@/service/authService";
+
 
 const MENU_ITEMS = [
   { name: "Dashboard", icon: LayoutDashboard, path: "/" },
